011ff22dde1d61451631e2f78cb159d45b0dbe46
Ryan Wholey <ryan@takescoop.com>
2022-01-28 16:24:55 -0700

    init

---
+++ main.tf
@@ -0,0 +1,35 @@
+
+resource "aws_kms_key" "secrets" {}
+
+data "aws_iam_document_policy" "decrypt" {
+  statement {
+    actions   = ["kms:Decrypt"]
+    resources = [aws_kms_key.secrets.id]
+  }
+}
+
+resource "aws_iam_user" "updater" {
+  name = "terraform-workspace-updater"
+}
+
+resource "aws_iam_access_key" "updater" {
+  user = aws_iam_user.updater.name
+}
+
+resource "aws_iam_user_policy" "updater" {
+  name   = "terraform-workspace-updater"
+  user   = aws_iam_user.updater.name
+  policy = data.aws_iam_document_policy.decrypt.json
+}
+
+output "aws" {
+  value = {
+    key_id     = aws_iam_access_key.updater.id
+    secret_key = aws_iam_access_key.updater.secret
+  }
+}
+
+output "key_id" {
+  value = aws_kms_key.secrets.id
+}
+
